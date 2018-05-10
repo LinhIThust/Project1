3a0ab482dfeed4dde38b87cc8a1a0511eee95300
LinhIThust <32013344+LinhIThust@users.noreply.github.com>
2018-05-10 10:38:06 +0700

    ..

--- QLTV.cpp
+++ QLTV.cpp
@@ -2,9 +2,13 @@
 #define MAXN 300
 #define MAXName 34
 #define F(i,a,b) for(int i =a;i<=b;i++)
-
+#define ID ID
+#define age age
+#define groupID groupID
+//----------------------------------------//
 using namespace std;
 int num=0;
+
 struct members{
 	int  ID;
 	char name[MAXName];
@@ -12,17 +16,15 @@ struct members{
 	int groupID;
 };
 
-FILE*f = fopen("output.txt","a+");
-
 members member[MAXN];
-
-void addFile(){
-	fprintf(f,"thanh vien thu %d :\n",num);
-	fprintf(f,"%d\n",member[num].groupID);
-	fprintf(f,"%d\n",member[num].ID);
-	fprintf(f,"%d\n",member[num].age);
-	fprintf(f,"%s\n",member[num].name);
-}
+//
+//void addFile(){
+//	fprintf(f,"thanh vien thu %d :\n",num);
+//	fprintf(f,"%d\n",member[num].groupID);
+//	fprintf(f,"%d\n",member[num].ID);
+//	fprintf(f,"%d\n",member[num].age);
+//	fprintf(f,"%s\n",member[num].name);
+//}
 
 int findMember(int id){
 	for(int i =1;i<=num;i++){
@@ -31,15 +33,31 @@ int findMember(int id){
 	}
 }
 
+bool checkID(int id){
+	for(int i = 1; i<=num;i++)
+		if(member[i].ID == id)
+			return false;
+ 	return true;
+}
+
 void addMenber(){
-	cout<<"NOTE: nhan 1 de ngung nhap!!"<<endl;
+	cout<<"NOTE: nhan -1 de ngung nhap!!"<<endl;
 	while(1){
+		int id;
 		num++;
-		cout<<"Nhap ID:";
-		cin >>member[num].ID ;
-		if(member[num].ID == -1){
-			num--;
-			break;
+		while(1){
+			cout<<"Nhap ID:";
+			cin >>id;
+			if(id == -1){
+				num--;
+				return;
+			}
+			if(!checkID(id)){
+				cout<<"ID ban vua nhap vua bi trung!Hay nhap lai:"<<endl;
+			} else{
+				member[num].ID = id;
+				break;
+			}
 		}
 		cout<<"Nhap tuoi:";
 		cin >>member[num].age;
@@ -48,20 +66,7 @@ void addMenber(){
 		cout<<"Nhap ten:";
 		fflush(stdin);
 		gets(member[num].name);
-		addFile();
 	}
-}
-
-void printMember(){
-	int id;
-	cout<<"Nhap ID:";
-	cin>> id;
-	int i = findMember(id);
-	cout<<"============================="<<endl;
-	cout<<"ID  :" <<member[i].ID << endl;
-	cout<<"Ten :" <<member[i].name << endl;
- 	cout<<"Tuoi:" <<member[i].age << endl;
- 	cout<<"Nhom:" <<member[i].groupID <<endl;
 }
 
 void deleteMember(){
@@ -73,10 +78,10 @@ void deleteMember(){
 		cout<<"thanh vien khong ton tai!"<<endl;
 		return;
 	}
-	member[i].ID =-1;
-	//member[i].name=32;
-	member[i].age =0;
-	member[i].groupID=0;
+	for(int j = i+1 ;j<=num;j++){
+		member[j-1] = member[j];
+	}
+	num--;
 }
 
 void editMember(){
@@ -96,27 +101,73 @@ void editMember(){
 
 }
 
+void printMember(){
+	int id;
+	cout<<"Nhap ID:";
+	cin>> id;
+	int i = findMember(id);
+	cout<<"============================="<<endl;
+	cout<<"ID  :" <<member[i].ID << endl;
+	cout<<"Ten :" <<member[i].name << endl;
+ 	cout<<"Tuoi:" <<member[i].age << endl;
+ 	cout<<"Nhom:" <<member[i].groupID <<endl;
+}
+
+void printAllMember(){
+	cout<<"so thanh vien trong Doi la:"<<num<<endl;
+	printf("ID              NAME               AGE    GROUP");
+	printf("\n");
+	for(int i =1;i<=num;i++){
+	printf("%6d%25s%6d%6d\n",member[i].ID,member[i].name,member[i].age,member[i].groupID);
+	}
+}
+
+void Quick_sort(int L,int R){
+	int i,j,pivot;
+	i = L;
+	j = R;
+	pivot = member[L+rand()%(R-L+1)].ID;
+	do{
+		while(member[i].ID<pivot) i++;
+		while(member[j].ID>pivot) j--;
+		if(i<=j){
+			swap(member[i],member[j]);
+			i++;j--;
+		}
+	}while( i<=j);
+	if(i<R) Quick_sort(i,R);
+	if(j>L) Quick_sort(L,j);
+}
+
+void SapXepID(){
+	cout<<"danh sach Thanh vien Theo ID tang dan!"<<endl;
+	Quick_sort(1,num);
+	printAllMember();
+ }
+
 void Menu(){
-	cout<<"||------------------------------------------||"<<endl;
-	cout<<"||>>>>>>>HE THONG QUAN LI THANH VIEN<<<<<<<<||"<<endl;
-	cout<<"||     Them thanh vien moi      : 1         || "<<endl;
-	cout<<"||     Xoa thanh vien cu        : 2         ||"<<endl;
-	cout<<"||     Sua thong tin thanh vien : 3         ||"<<endl;
-	cout<<"||     In thanh vien Doi theo ID: 4         ||"<<endl;
-	cout<<"||------------------------------------------||"<<endl;
+	cout<<"||---------------------------------------------||"<<endl;
+	cout<<"||>>>>>>>>>HE THONG QUAN LI THANH VIEN<<<<<<<<<||"<<endl;
+	cout<<"||     Them thanh vien moi         : 1         || "<<endl;
+	cout<<"||     Xoa thanh vien cu           : 2         ||"<<endl;
+	cout<<"||     Sua thong tin thanh vien    : 3         ||"<<endl;
+	cout<<"||     In thanh vien Doi theo ID   : 4         ||"<<endl;
+	cout<<"||     In tat ca thanh vien Doi    : 5         ||"<<endl;
+	cout<<"||     Sap xep thanh vien theo ID  : 6         ||"<<endl;
+	cout<<"||---------------------------------------------||"<<endl;
 	cout<<"   ==> Nhan de chon chuc nang   : ";
 	int option;
 	cin>>option;
 	switch(option){
 		case 1 : addMenber(); break;
-		case 2 : deleteMember; break;
+		case 2 : deleteMember(); break;
 		case 3 : editMember(); break;
 		case 4 : printMember();break;
+		case 5 : printAllMember();break;
+		case 6 : SapXepID();break;
 		default: cout<<"Vui long nhap lai!"<<endl;
 	}
-
 }
-
 
 int main(){
 	char ch;
@@ -127,7 +178,6 @@ int main(){
 		system("cls");
 
 	}while( ch =='Y'||ch =='y');
-
 	cout<<"Cam on ban da su dung ung dung"<<endl;
 	cout<<"----Copyright :Linhdzai--------";
 	return 0;
